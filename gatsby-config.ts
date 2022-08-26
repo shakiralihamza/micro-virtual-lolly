643c5e6cc46cb384dba21ef91b922e74fe87fef9
Shakir Ali <shakiralihamza05@gmail.com>
2022-08-27 04:48:41 +0500

    initial configuration

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,36 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+      {
+          resolve: `gatsby-plugin-manifest`,
+          options: {
+              name: `Virtual Lolly App`,
+              short_name: `virtual_lolly`,
+              start_url: `/`,
+              background_color: `#f7f0eb`,
+              theme_color: `#3f4ab0`,
+              display: `standalone`,
+              icon: `src/images/lolly.svg`,
+          },
+      },
+      {
+          resolve: `gatsby-plugin-offline`,
+          options: {
+              precachePages: [`/`],
+          },
+      },
+      {
+          resolve: 'gatsby-plugin-react-svg',
+          options: {
+              rule: {
+                  include: /\.inline\.svg$/
+              }
+          }
+      },
+      'gatsby-plugin-top-layout',
+      'gatsby-plugin-mui-emotion',
+  ],
 }
 
 export default config
