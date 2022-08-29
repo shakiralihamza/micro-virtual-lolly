2864ec9c1a359ddb1ed31b034edc728663c161cf
Shakir Ali <shakiralihamza05@gmail.com>
2022-08-29 16:14:16 +0500

    test

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -26,7 +26,7 @@ const NotFoundPage = () => {
     console.log('lollyID: ', lollyID);
     useEffect(() => {
         console.log('UE lollyID: ', lollyID);
-        axios.post(`/.netlify/functions/showLolly/?lollyID=${lollyID}`)
+        axios.post(`/getLolly/?lollyID=${lollyID}`)
             .then(res => setLolly(res.data))
             .catch(() => setFound(false))
     }, [])
